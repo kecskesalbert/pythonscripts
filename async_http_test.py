a92562f4f4ce10a367794477e342d32fe43bbd31
Albert Kecskés <kecskesalbert@users.noreply.github.com>
2025-02-20 11:36:37 +0000

    Added http retry for async_http_test.py

--- async_http_test.py
+++ async_http_test.py
@@ -4,11 +4,9 @@
 # Output them as semicolon separeated csv: name, webpage, address, size
 
 # TODO:
-# Catch random httpx.ReadTimeout exceptions, retry
-# Limit concurrency, take argument as parameter
-# async follow redirects
+# Limit concurrency
 # use csv module for flexible output
-# Take timeout as parameter
+# async follow redirects
 
 import asyncio
 import httpx
@@ -19,29 +17,38 @@ import sys
 #from urllib.parse import urljoin
 from urllib import parse
 
+async def http_get(httpx_client, url, **kvargs):
+    success = False
+    while not success:
+        try:
+            print("REQ", url[:75], file=sys.stderr)
+            resp = await httpx_client.get(url, **kvargs)
+            success = True
+        except httpx.ReadTimeout:
+            print("Timeout occured, retrying", file=sys.stderr)
+    print("RCV", str(resp.url)[:75], resp.status_code, flush=True, file=sys.stderr)
+    return resp
+
+
 async def fetchpage(base_url='https://www.cambridgenetwork.co.uk/companies/search-results'):
     async with httpx.AsyncClient() as httpx_client:
         for prefix in ([0,] + list(range(65,91))):
-        #for prefix in (range(67,70)):
+        #for prefix in (range(82,91)):
             page_number = 0
             url = base_url + "?" + parse.urlencode({True:{'numeric': 1}, False: {'prefix': chr(prefix)}}[prefix==0])
             while True:
-                print("REQ", str(url)[:75], file=sys.stderr)
-                resp = await httpx_client.get(url, follow_redirects=True)
-                print("RSP", str(resp.url)[:75], resp.status_code, flush=False, file=sys.stderr)
+                resp = await http_get(httpx_client, url, follow_redirects=True)
                 lentilsoup = BeautifulSoup(resp.text, 'lxml')
 
                 # List of companies
                 for companyitem in lentilsoup.find_all(name='div', class_='views-row'):
-                    found = { "address": "", "webpage": "", "size": "", "name": "" }
+                    found = { "address": "", "webpage": "", "size": "", "name": "", "size_hint": "" }
                     companydiv = companyitem.find(name='div', class_= 'views-field-display-name-1').contents[0].contents[0]
                     url = parse.urljoin(base_url, companydiv['href'])
                     found["name"] = re.sub(r'\W+','', companydiv.string)
 
                     # Company showcase page
-                    print("REQ", url[:75], file=sys.stderr)
-                    resp = await httpx_client.get(url, follow_redirects=True)
-                    print("RSP", str(resp.url)[:75], resp.status_code, flush=True, file=sys.stderr)
+                    resp = await http_get(httpx_client, url, follow_redirects=True)
                     carrotsoup = BeautifulSoup(resp.text, 'lxml')
 
                     for detailitem in carrotsoup.find(name='div', class_='article-aside').find_all(name='div', class_='views-row'):
@@ -62,7 +69,10 @@ async def fetchpage(base_url='https://www.cambridgenetwork.co.uk/companies/searc
                             if (found["webpage"] == ""):
                                 found['webpage'] = detailitem.contents[0]['href']
                         else:
+                            #size_match = re.match(r"[^0-9 ]+(\d+)\s*$", detailitem.contents[0].string)
                             found['size'] = detailitem.contents[0].string
+                            #if (size_match):
+                            #    found['size_hint'] = size_match.groups()[0]
 
                     print(";".join((found["name"], found["webpage"], found["address"], found["size"])), flush=False)
 
